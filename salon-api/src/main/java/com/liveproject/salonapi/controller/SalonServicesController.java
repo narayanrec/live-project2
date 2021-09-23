65054d699217f0ba5aeea65eccbce2f82d64ac1e
Narayan Agarwal <narayanrec@gmail.com>
2021-09-22 22:08:57 -0500

    get slots api by date and serviceId

--- salon-api/src/main/java/com/liveproject/salonapi/controller/SalonServicesController.java
+++ salon-api/src/main/java/com/liveproject/salonapi/controller/SalonServicesController.java
@@ -1,14 +1,14 @@
 package com.liveproject.salonapi.controller;
 
 import com.liveproject.salonapi.domain.SalonServiceDetail;
+import com.liveproject.salonapi.domain.Slot;
 import com.liveproject.salonapi.repository.SalonServiceDetailRepository;
+import com.liveproject.salonapi.repository.SlotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping(path = "/api/services")
@@ -18,10 +18,20 @@ public class SalonServicesController {
     @Autowired
     SalonServiceDetailRepository salonServiceDetailRepository;
 
+    @Autowired
+    SlotRepository slotRepository;
+
     @GetMapping("/retrieveAvailableSalonServices")
     Iterable<SalonServiceDetail> retrieveSalonServices() {
 
         return salonServiceDetailRepository.findAll();
+
+    }
+
+    @GetMapping("/retrieveAvailableSlots/{salonServiceId}/{requestDate}")
+    Iterable<Slot> retrieveAvailableSlots(@PathVariable long salonServiceId, @PathVariable("requestDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE, pattern = "yyyy-MM-dd") LocalDate requestDate) {
+
+        return slotRepository.getAllSlots(salonServiceId, requestDate);
 
     }
 }
